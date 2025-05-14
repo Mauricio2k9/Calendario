dc8a8a87eed93cee680ae31e5cb0df95ae385abc
Mauricio2k9 <mirac.info@gmail.com>
2025-05-14 15:04:48 -0400

    pagina funciona

--- ReservaCalendarioApp/urls.py
+++ ReservaCalendarioApp/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.index, name = 'index'),
-
-
+    path('', views.index, name='index'),  # Ensure this is defined
 ]
