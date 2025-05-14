ba53fea19586c9ea5713c6357aa210557d313e7d
IanHazecure <136367549+IanHazecure@users.noreply.github.com>
2025-05-14 14:46:04 -0400

    working test

--- ReservaCalendario/ReservaCalendario/urls.py
+++ ReservaCalendario/ReservaCalendario/urls.py
@@ -17,10 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views  # Import the views module from the current directory
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
     path('', include('ReservaCalendarioApp.urls')),
 ]
