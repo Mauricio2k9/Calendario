771c734af095c5dfdf48eaf4e84fd94cf86e5462
Mauricio2k9 <mirac.info@gmail.com>
2025-05-14 13:11:51 -0400

    1
    11

--- ReservaCalendario/ReservaCalendario/urls.py
+++ ReservaCalendario/ReservaCalendario/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from . import views  # Import the views module from the current directory
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('', include('ReservaCalendarioApp.urls')),
 ]
